849174a0c0b0886e8ec60ceda3ac6bac5f41117c
Jam Kay <jam@jimkay.us>
2020-05-13 17:48:43 -0700

    Fixed problems with getting Cangie

--- TraditionalChineseManage/Form1.cs
+++ TraditionalChineseManage/Form1.cs
@@ -291,7 +291,7 @@ namespace TraditionalChineseManage
 			if (txtCji1.Text.Length < 1)
 				return;
 
-			IList<char> fromCjis;
+			string fromCjis;
 
 			var success = Traditional.TryGetCjiChar(txtCji1.Text, out fromCjis);
 
